b8e842fc1bfb721a1e2d024d71e14071201239b1
Jorge Jordan <j.jordanarenas@gmail.com>
2025-10-21 08:24:21 +0200

    Initial Commit

---
+++ WordPath/WordPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordPathApp.swift
+//  WordPath
+//
+//  Created by Jorge Jordán on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WordPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordPathTests/WordPathTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordPathTests.swift
+//  WordPathTests
+//
+//  Created by Jorge Jordán on 21/10/25.
+//
+
+import Testing
+@testable import WordPath
+
+struct WordPathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
